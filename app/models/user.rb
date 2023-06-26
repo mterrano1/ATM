4d91e0cf5ab19297d0e0d03738faa3c1f9544e46
Christopher Terrano <mterrano1@yahoo.com>
2023-06-25 22:22:07 -0500

    create user model methods, tests, and added shoulda helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_one :account
+
+  has_secure_password
+
+  validates :username, :first_name, :last_name, :email, presence: true
+  validates :username, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created successfully with valid data" do
+    user = User.create(username: "test_user", 
+      password: "sup3r-secret", 
+      first_name: "John", 
+      last_name: "Doe", 
+      email: "johndoe@gmail.com")
+    expect(user).to be_valid
+  end
+
+  it "has one account" do
+    user = User.reflect_on_association(:account)
+    expect(user.macro).to eq(:has_one)
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:email) }
+    
+    it { is_expected.to validate_uniqueness_of(:username) }
+  end
+
+  describe "authenticate" do
+    it "returns the user if credentials match" do
+      user = User.create(username: "test_user", 
+        password: "sup3r-secret", 
+        first_name: "John", 
+        last_name: "Doe", 
+        email: "johndoe@gmail.com")
+      expect(user.authenticate("sup3r-secret")).to eq(user)
+    end
+
+    it "returns false if credentials don't match" do
+      user = User.create(username: "test_user", 
+        password: "sup3r-secret", 
+        first_name: "John", 
+        last_name: "Doe", 
+        email: "johndoe@gmail.com")
+      expect(user.authenticate("nope")).to be(false)
+    end
+  end
 end
