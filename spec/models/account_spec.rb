96325eefa047885e7b737bf339a0fe3056a0623d
Christopher Terrano <mterrano1@yahoo.com>
2023-06-25 23:30:50 -0500

    create account model test

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when creating a new account' do
+    it 'creates a new account with the correct user and starting balance' do
+      user = User.create(username: "test_user", password: "sup3r-secret", first_name: "John", last_name: "Doe", email: "johndoe@gmail.com")
+      account = Account.new(user_id: user.id, balance: 1000000)
+
+      expect(account).to be_valid
+    end
+  end
 end
